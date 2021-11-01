2b5aa55c113eb02e0d1a61f57ef8ead9b9a29b02
jeffberlin <jeff@jbrally.com>
2021-11-01 15:58:31 -0400

    Building

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const generalInformationArray = [
+  {
+    "question": "What is BMT Micro?",
+    "answer": "BMT Micro is a software distribution and order fulfillment service for professional through part time software developers."
+  }
+]
