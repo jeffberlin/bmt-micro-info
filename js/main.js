ec6002ec69df9698ec9be8c46d51431baa359f0b
Jeff Berlin <jeff@jbrally.com>
2021-11-10 16:03:49 -0500

    Clearing JS file

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const generalInformationArray = [
-  {
-    "question": "What is BMT Micro?",
-    "answer": "BMT Micro is a software distribution and order fulfillment service for professional through part time software developers."
-  }
-]
