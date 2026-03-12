18167d81a4af697f58d5c408f3f206c09d8fffef
Jeff Berlin <jeff@jbrally.com>
2026-03-12 19:24:58 -0400

    Updating unsubscribe verbiage

--- bmt-info-website/src/Components/CustomerUnsubscribe.js
+++ bmt-info-website/src/Components/CustomerUnsubscribe.js
@@ -15,7 +15,8 @@ function CustomerUnsubscribe() {
             <h4 className="page-title">Unsubscribe</h4>
           </Col>
           <Col xl={10} lg={12} md={12} sm={12}>
-            <p className='body-text'>The email that you received from BMT Micro is a one-time mailing in direct response to a product or service you purchased from us When we sell a product, we send out a purchase payment receipt and product fulfillment information. BMT MICRO DOES NOT MAINTAIN MAILING LISTS, so there is no need to unsubscribe.</p>
+            <p>Request for data removal</p>
+            <p className='body-text'>The email that you received from BMT Micro is a one-time mailing in direct response to a product or service you purchased from us When we sell a product, we send out a purchase payment receipt and product fulfillment information. <strong>BMT MICRO DOES NOT MAINTAIN MAILING LISTS, so there is no need to unsubscribe</strong>.</p>
             <p className='body-text'>However, should you want us to delete your customer account at <a href="https://customers.bmtmicro.com" target="_blank" rel="noreferrer">https://customers.bmtmicro.com</a>, please contact <a href="mailto:webmaster@bmtmicro.com">webmaster@bmtmicro.com</a> and request data removal as per GDPR.</p>
           </Col>
         </Row>
--- bmt-info-website/src/Components/VendorAffiliateUnsubscribe.js
+++ bmt-info-website/src/Components/VendorAffiliateUnsubscribe.js
@@ -15,7 +15,8 @@ function VenAffUnsubscribe() {
             <h4 className="page-title">Unsubscribe</h4>
           </Col>
           <Col xl={10} lg={12} md={12} sm={12}>
-            <p className='body-text'>The email that you received from BMT Micro is a one-time mailing in direct response to a product or service you sold via us. When we sell a product, we send out various emails to the supplier. BMT MICRO DOES NOT MAINTAIN MAILING LISTS, so there is no need to unsubscribe.</p>
+            <p>Help me configure notifications for my seller account</p>
+            <p className='body-text'>The email that you received from BMT Micro is a one-time mailing in direct response to a product or service you sold via us. When we sell a product, we send out various emails to the supplier. <strong>BMT MICRO DOES NOT MAINTAIN MAILING LISTS, so there is no need to unsubscribe</strong>.</p>
             <p className='body-text'>If you feel the emails you are receiving are not relevant or you just do not want to receive them, then please log in to your vendor or affiliate area and configure your notifications accordingly. You may also contact <a href="mailto:vendors@bmtmicro.com">vendors@bmtmicro.com</a> for help with configuring your vendor or affiliate account.</p>
           </Col>
         </Row>
